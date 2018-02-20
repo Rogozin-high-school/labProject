bd71487314fd8c42863b40b2a1398c1ca71d9891
or navon <navonor@gmail.com>
2018-02-20 17:14:32 +0200

    better example

--- lab2/lab2/index.py
+++ lab2/lab2/index.py
@@ -2,6 +2,8 @@ from server import handler,run
 
 @handler("index")
 def printL(req):
-    return 200,{},"hello world"
+    with open('index.html', 'r') as myfile:
+        data=myfile.read().replace('\n', '')
+    return 200,{"Content-type":"text/html"},data #return_code,headers,code of the page(here it is html)
 
 run()
