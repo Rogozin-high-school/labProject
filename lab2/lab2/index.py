2e60bf85e7f8ca58a030cbbdaa4729ba67f7d44f
StavAlush27 <stavalush27@gmail.com>
2018-03-11 16:23:32 +0200

    issue 2 complete

--- lab2/lab2/index.py
+++ lab2/lab2/index.py
@@ -2,13 +2,13 @@ from server import handler,run
 
 @handler("index")
 def printL(req):
-    with open('index.html', 'r') as myfile:
+    with open('WebPage1.html', 'r') as myfile:
         data=myfile.read().replace('\n', '')
     return 200,{"Content-type":"text/html"},data #return_code,headers,code of the page(here it is html)
 
 @handler("index","POST")
 def answerpost(req,):
-    with open('index.html', 'r') as myfile:
+    with open('WebPage1.html', 'r') as myfile:
         data=myfile.read().replace('\n', '')
     return 200,{"Content-type":"text/html"},data #return_code,headers,code of the page(here it is html)
 
