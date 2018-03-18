caa198817d849ad3b827e276c77ff9c6964ab9c0
unknown <shaikimhi221199@gmail.com>
2018-03-18 07:48:22 +0200

    fixed and checked field

--- lab2/lab2/Field.py
+++ lab2/lab2/Field.py
@@ -3,8 +3,4 @@ import math
 
 def circular_field(angle:float)->numpy.ndarray:
 	angle = math.degrees(angle)
-	l = 1
-	a = math.sqrt(1 / math.cos(angle)**2 - 1)
-	x = math.sqrt(a**2 / (1 + a**2))
-	y = math.sqrt(1 / 1 + a**2)
-	return numpy.ndarray([x,y,0])
+	return numpy.ndarray([math.sin(angle),math.cos(angle)])
