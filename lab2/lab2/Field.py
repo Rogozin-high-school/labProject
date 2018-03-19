6a388475e0c27883a076e2e0b0d3c65dbf24546e
unknown <shaikimhi221199@gmail.com>
2018-03-19 08:38:48 +0200

    updated field

--- lab2/lab2/Field.py
+++ lab2/lab2/Field.py
@@ -1,18 +1,28 @@
 import numpy
 import math
-
+import time
 FIELD_SIZE = 10
 
 def circular_field(angle:float)->numpy.ndarray:
-	angle = math.degrees(angle)
+	angle = math.radians(angle)
 	a = math.sin(angle)
 	b = math.cos(angle)
 	x = None
 	y = None
 	if(b!=0):
-		x = math.sqrt(10 / ((a**2 / b**2) + 1))
-		y = -1 * a * x / b
+		x = 1
+		y = -a/b
+		arr = numpy.ndarray([x,y])
+		arr.reize(1)
+		return [arr[0],arr[1]]
 	else:
+		print("\n\n\n")
 		x = 0
 		y = a * FIELD_SIZE
-	return numpy.ndarray([math.sin(angle),math.cos(angle)])
+	return [x,y]
+
+
+for i in range(0,360,10):
+	time.sleep(0.1)
+	B = circular_field(i)
+	print(str(i)+"-> "+str(B[0])+":"+str(B[1]))
