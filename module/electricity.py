c1016049e71d6a64bdcd1383b829ac7291a3081c
Yotam Salmon <yotam.salmon@gmail.com>
2018-03-23 16:19:54 +0300

    Structure the v2c (vector to current) module

---
+++ module/electricity.py
@@ -0,0 +1,16 @@
+"""
+The electricity module should help us with the mathematics required for converting physical
+quantities (as magnetic field vectors) to the required electrical quantities (voltage, current).
+"""
+
+# Mathematical & Physical constants that can help us later
+π = 3.1415926535
+μ0 = 4e-7 * π
+
+def coil_field(coil_radius, coil_length, current, N):
+    """ To calculate the field of a Helmholtz coil and return the magnitude of the field"""
+    pass
+
+def field_coil(coil_radius, coil_length, N, field):
+    """ To calculate the current required for creating a magnetic field. Return value in Amperes """
+    pass
