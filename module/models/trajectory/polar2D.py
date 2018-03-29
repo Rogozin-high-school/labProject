6049a224da428159ff92bbc27ca347a551676ced
Yotam Salmon <yotam.salmon@gmail.com>
2018-03-29 16:11:30 +0300

    Polar field 2D equations

---
+++ module/models/trajectory/polar2D.py
@@ -0,0 +1,17 @@
+from .base import Trajectory
+import numpy as np
+
+class PolarTrajectory2D(Trajectory):
+    """
+    Mathematical model of a polar trajectory
+    """
+    def __init__(self, altitude, circle_time):
+        """
+        Altitude - height above the center of the planet
+        """
+        self.altitude = altitude
+        self.angular_velocity = 360 / circle_time
+
+    def disposition(self, t) -> np.ndarray:
+        return np.array([(self.angular_velocity * t) % 360, self.altitude])
+        
