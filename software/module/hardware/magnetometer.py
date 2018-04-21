dcc635559deb9e65a5a2f0e1016157d647ca53db
Yotam Salmon <yotam.salmon@gmail.com>
2018-04-21 15:59:04 +0300

    Magnetometer module now queries data

--- software/module/hardware/magnetometer.py
+++ software/module/hardware/magnetometer.py
@@ -4,27 +4,37 @@ Infrastructure:
     Computer <-> Arduino <-> Magnetometer
 """
 
+import serial
+import serial.tools.list_ports as lp
+
+import time
+
 class Magnetometer(object):
     """ A class that handles the reading of a magnetometer sensor """
 
     def __init__(self, comport=None):
         """ Sets a new connection to a magnetometer """
-        self.comport = comport
+        self.comport = comport or self.__find_comport()
 
     def __find_comport(self):
         """ Auto-finds an available COM port that responds to the magnetometer ping command. """
-        raise NotImplementedError\
-            ("The __find_comport method of the class Magnetometer was not yet implemented.")
+        
+        avail = [x.device for x in lp.comports() if "Arduino Uno" in x.description]
+        return avail[0] if avail else None
 
     def connect(self):
         """ Connects to the magnetometer port """
-        raise NotImplementedError\
-            ("The connect method of the class Magnetometer was not yet implemented.")
+        self.ser = serial.Serial(self.comport, 9600)
+        self.ser.close()
+        self.ser.open()
 
     def get_field(self):
         """ Returns the magnetometer reading as a numpy array of length 3 """
-        raise NotImplementedError\
-            ("The get_field method of the class Magnetometer was not yet implemented.")
+        self.ser.write(b"F")
+        time.sleep(0.01)
+        if self.ser.in_waiting == 0:
+            return None
+        return [float(i.strip()) for i in str(self.ser.readline(), "ascii").split(",")]
 
     def get_horizontal_direction(self):
         """ Returns the angle across the [xy] plane of the magnetic field vector """
