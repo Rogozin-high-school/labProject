21261e3c9c4a7755b35dc9ae6e6fa4e453045f20
shaiKemhi <shaikimhi221199@gmail.com>
2018-07-12 16:40:04 +0300

    dipolar field

--- software/module/models/field/dipolar.py
+++ software/module/models/field/dipolar.py
@@ -1,9 +1,25 @@
 from .base import Field
 import numpy as np
+import math
 
-class DipolarField(Field):
+from ..physics.electricity import μ0
+
+class DipolarField2D(Field):
     """
     The mathematical model of a Dipole based field.
     """
-    def field(self, vec3) -> np.ndarray:
-        print("Dipole") 
+    def __init__(self, moment):
+        self.moment = moment
+
+    def field(self, vec2) -> np.ndarray:
+        angle = math.radians(vec2[0])
+        c = math.cos(angle)
+        s = math.sin(angle)
+        m = length(self.moment)
+        return numpy.array([
+        3 * c * s,
+        3 * c ** 2 - 1
+        ]) * m * μ0 / vec2[1] ** 3
+
+    def length(vec):
+        return math.sqrt(sum(vec))
